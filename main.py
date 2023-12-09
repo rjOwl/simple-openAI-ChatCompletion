160cb7589be9e86dcdb887c5e6daa11615fe9ed9
mostafa <mostafa>
2023-12-09 16:34:08 +0200

    creat initial logic

---
+++ main.py
@@ -0,0 +1,22 @@
+'''example completion with openai > 1.1'''
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This line brings all environment variables from .env into os.environ
+OPEN_AI_KEY = os.environ['OPEN_AI_KEY']
+client = OpenAI(api_key=OPEN_AI_KEY)
+# prompt = "True or false: a banana is smaller than a lemon.\n\n"
+
+while True:
+    prompt = input("Type your propmt: ")
+    response = client.completions.create(
+        prompt=prompt,
+        model="gpt-3.5-turbo-instruct",
+        top_p=0.5, max_tokens=1500,
+        stream=True)
+    # print(response)
+    res = ""
+    for part in response:
+        res+=part.choices[0].text + " "
+    print(res)
