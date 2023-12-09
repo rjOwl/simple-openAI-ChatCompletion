a55943db45197fb3a05a70874e8b4666d30a04e9
mostafa <mostafa>
2023-12-09 16:36:16 +0200

    reomve comments

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 load_dotenv()  # This line brings all environment variables from .env into os.environ
 OPEN_AI_KEY = os.environ['OPEN_AI_KEY']
 client = OpenAI(api_key=OPEN_AI_KEY)
-# prompt = "True or false: a banana is smaller than a lemon.\n\n"
 
 while True:
     prompt = input("Type your propmt: ")
@@ -15,7 +14,7 @@ while True:
         model="gpt-3.5-turbo-instruct",
         top_p=0.5, max_tokens=1500,
         stream=True)
-    # print(response)
+
     res = ""
     for part in response:
         res+=part.choices[0].text + " "
